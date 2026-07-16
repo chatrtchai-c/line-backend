5a4df01351693af0c79f387fd187d6ccb01f0023
chatrtchai-c <chatrtchai.c@gmail.com>
2026-07-16 09:59:15 +0700

    feat: Reset project

--- v1/server.js
+++ v1/server.js
@@ -3,73 +3,15 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const express = require('express');
 const cors = require('cors');
-const { middleware } = require('@line/bot-sdk');
-const { sendBirthdayMessage, sendWelcomeMessage, handleEvent } = require('./services/lineService');
-require('./cron/cron');
+
 
 const app = express();
 
 app.use(cors());
 
-// Webhook for LINE Message API
-app.post('/webhook', middleware({ channelSecret: process.env.LINE_CHANNEL_SECRET }), async (req, res) => {
-    try {
-        const events = req.body.events;
-
-        if (!events || !Array.isArray(events)) {
-            return res.status(400).json({ error: "Invalid payload: events array is required" });
-        }
-
-        // Process all events in parallel
-        const results = await Promise.all(events.map(handleEvent));
-
-        return res.json(results);
-    } catch (error) {
-        console.error("Error handling LINE webhook events:", error);
-        return res.status(500).end();
-    }
-});
-
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const API_VERSION = process.env.API_VERSION;
-
-// Define Route
-app.post(`/api/${API_VERSION}/send-birthday-message`, async (req, res) => {
-    try {
-        const { userId, name, message } = req.body;
-
-        if (!userId) {
-            return res.status(400).json({ error: "กรุณาระบุ LINE User ID (userId)" });
-        }
-
-        await sendBirthdayMessage(userId, name, message);
-
-        return res.json({ success: true, message: `ส่งข้อความอวยพรวันเกิดสำเร็จไปยัง User ID: ${userId}` });
-    } catch (error) {
-        console.error("Error sending LINE message:", error);
-        return res.status(500).json({ error: "เกิดข้อผิดพลาดในการส่งข้อความ LINE", details: error.message });
-    }
-});
-
-app.post(`/api/${API_VERSION}/send-welcome-message`, async (req, res) => {
-    try {
-        const { lineUuid, displayName } = req.body;
-
-        if (!lineUuid) {
-            return res.status(400).json({ error: "กรุณาระบุ LINE User ID (userId)" });
-        }
-
-        console.log(`[Backend] Send welcome message. to ${displayName} (${lineUuid}) `);
-        await sendWelcomeMessage(lineUuid, displayName);
-
-        return res.json({ success: true, message: `ส่งข้อความต้อนรับสำเร็จไปยัง User ID: ${lineUuid}` });
-    } catch (error) {
-        console.error("Error sending LINE welcome message:", error);
-        return res.status(500).json({ error: "เกิดข้อผิดพลาดในการส่งข้อความ LINE", details: error.message });
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`Server starting ... on Port ${PORT}`);
