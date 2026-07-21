2066b511a0682c33cb80f65d850688318334adcd
chatrtchai-c <chatrtchai.c@gmail.com>
2026-07-21 15:09:10 +0700

    fix: liff url

--- v1/controllers/webhook.controller.js
+++ v1/controllers/webhook.controller.js
@@ -23,7 +23,7 @@ const handleEvent = async (event) => {
   }
 
   if (event.message.text === 'ตรวจสอบสิทธิการลา') {
-    const liffUrl = `${process.env.LINE_LIFF_ID}/pin`;
+    const liffUrl = `https://liff.line.me/${process.env.LINE_LIFF_ID}/pin`;
     const flexMessage = generateRequirePinFlex(liffUrl);
     
     return client.replyMessage({
